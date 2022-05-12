d3e7d924d1d5d561ed9bba9195384f2ceb38864d
emchachi <emiliyanhristov99@gmail.com>
2022-05-12 19:50:18 +0200

    image carousel

--- js/script.js
+++ js/script.js
@@ -27,3 +27,71 @@ btnNav.addEventListener("click", function () {
     hideDesc.classList.remove("hide");
   }
 });
+
+// IMAGE CAROUSEL
+
+const images = document.querySelectorAll(".gallery-img");
+let imgIndex;
+let imgSrc;
+
+images.forEach((img, i) => {
+  img.addEventListener("click", (e) => {
+    imgSrc = e.target.src;
+
+    imgModal(imgSrc);
+
+    imgIndex = i;
+  });
+});
+
+let imgModal = (src) => {
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+
+  document.querySelector(".section-gallery").append(modal);
+
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", src);
+
+  const closeBtn = document.createElement("i");
+  closeBtn.setAttribute("class", "fas fa-times closeBtn");
+
+  closeBtn.onclick = () => {
+    modal.remove();
+  };
+
+  const nextBtn = document.createElement("i");
+  nextBtn.setAttribute("class", "fas fa-angle-right nextBtn");
+
+  nextBtn.onclick = () => {
+    newImage.setAttribute("src", nextImg());
+  };
+  const prevBtn = document.createElement("i");
+  prevBtn.setAttribute("class", "fas fa-angle-left prevBtn");
+
+  prevBtn.onclick = () => {
+    newImage.setAttribute("src", prevImg());
+  };
+  modal.append(newImage, closeBtn, nextBtn, prevBtn);
+};
+
+let nextImg = () => {
+  imgIndex++;
+
+  if (imgIndex >= images.length) {
+    imgIndex = 0;
+  }
+
+  return images[imgIndex].src;
+};
+
+let prevImg = () => {
+  imgIndex--;
+  console.log(imgIndex);
+
+  if (imgIndex < 0) {
+    imgIndex = images.length - 1;
+  }
+
+  return images[imgIndex].src;
+};
