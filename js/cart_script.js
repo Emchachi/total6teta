38997eea6f3615ab1b179e418e17c77018e275f2
emchachi <emiliyanhristov99@gmail.com>
2022-05-16 16:43:23 +0200

    form

--- js/cart_script.js
+++ js/cart_script.js
@@ -117,3 +117,10 @@ const closeModal = function (e) {
 
 btnOpenModal.addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
+
+function onlyNumberKey(evt) {
+  // Only ASCII character in that range allowed
+  var ASCIICode = evt.which ? evt.which : evt.keyCode;
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) return false;
+  return true;
+}
